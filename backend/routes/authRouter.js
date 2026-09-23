de9990c63706f414c6bc330de84c03db6910759f
luca-thompson <lucathompson006@gmail.com>
2026-09-23 16:43:04 +1000

    working-ish oauth

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -6,8 +6,8 @@ authRouter.get('/auth/github', (req, res) => {
   const auth_url =
     `https://github.com/login/oauth/authorize` +
     `?client_id=${process.env.GITHUB_CLIENT_ID}` +
-    `&scope=${process.env.SCOPE}` +
-    `&redirect_uri=${process.env.REDIRECT_URI}`;
+    `&scope=${process.env.GITHUB_SCOPE}` +
+    `&redirect_uri=${process.env.GITHUB_CALLBACK_URL}`;
 
   res.redirect(auth_url);
 });
